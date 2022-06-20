1dd46be3797a981d1fe3b0bc15d753144205b821
Ana Muraya <Anamuraya9@gmail.com>
2022-06-20 12:25:46 +0300

    Connected to the database

--- config/database.js
+++ config/database.js
@@ -5,12 +5,7 @@ const {MONGO_URI} = process.env;
 exports.connect = async () => {
     // Connect to MongoDB
     try {
-        await mongoose.connect(MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-        });
+        await mongoose.connect(MONGO_URI);
         console.log('MongoDB connected');
     } catch (error) {
         console.log(error);
